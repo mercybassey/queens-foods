f2c0b280650eb126bbd4a986885093964889aaf9
mercybassey <udohmercy911@gmail.com>
2020-07-29 13:39:22 -0700

    updating the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/hompage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const PizzaPage = () =>(
-  <div>
-    <h1>PizzaPage</h1>
-  </div>
 
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/pizza" component={PizzaPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,113 @@
+const SHOP_DATA = [
+
+    {
+        id: 1,
+        title: "Pizza",
+        routeName: 'pizza',
+        items: [
+            {
+                id: 1,
+                name: 'Baked Pizza',
+                image: '/images/pizza/baked-pizza.jpg',
+                price: '$10',
+            },
+            {
+                id: 2,
+                name: 'Brown Pizza',
+                image: '/images/pizza/brown-pizza.jpg',
+                price: '$12',
+            },
+            {
+                id: 3,
+                name: 'Pizza with Vegetables',
+                image: '/images/pizza/pizza-with-green-leaf-vegetable.jpg',
+                price: '$7',
+            },
+            {
+                id: 4,
+                name: 'Tomato Pizza',
+                image: '/images/pizza/tomato.jpg',
+                price: '$10',
+            }
+
+        ]
+    },
+    {
+        id: 2,
+        title: "Cakes",
+        routeName: 'cakes',
+        items: [
+            {
+                id: 1,
+                name: "Yellow-Butter cake",
+                image: "/images/cakes/3-layer-yellow-butter-cake.jpg",
+                price: "$10",
+            },
+            {
+                id: 2,
+                name: "Carrot Cake",
+                image: "/images/cakes/carrot-cake.jpg",
+                price: "$12"
+            },
+            {
+                id:3,
+                name: "Chiffon Cake",
+                image: "/images/cakes/chiffon-cake.jpg",
+                price: "$10"
+            },
+            {
+                id:4,
+                name: "Chocolate-Swirl-Pound Cake",
+                image: "/images/cakes/chocolate-matcha-swirl-pound-cake.jpg",
+                price: " $10",
+            },
+            {
+                id: 5,
+                name: " Raspberry Cake",
+                image:"/images/raspberry-pink-velvet-cake",
+                price:"$9"
+            }
+
+        ]
+    },
+    {
+        id: 3,
+        title: " Salad ",
+        routName: "salad",
+        items: [
+            {
+                id: 1,
+                name: "Asian Slaw",
+                image: "/images/salad/asian-slaw.jpg",
+                price:"$7"
+            },
+            {
+                id:2,
+                name: "Broccoli Salad",
+                image: "/images/salad/broccoli-salad.jpg",
+                price: "$8"
+            },
+            {
+                id:3,
+                name: " Brussels Salad",
+                image: "/images/salad/brussels-salad.jpg",
+                price: "$6"
+            },
+            {
+                id:4,
+                name: "Orzo Salad",
+                image: "/images/salad/orzo-salad.jpg",
+                price: "$8"
+            },
+            {
+                id:5,
+                name: "Romain-Wedges Salad",
+                iamge: "/images/salad/romain-wedges.jpg",
+                price: "$9"
+            }
+        ]
+    },
+    
+]
+
+export default SHOP_DATA;
