c10612bb175efc6aefea511449b7cc1bf8fbf898
mercybassey <udohmercy911@gmail.com>
2020-08-21 12:35:20 -0700

    added checkout page, checkout item component and updated cart to use selector

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ( {cartItem: { name, image, price, quantity} }) => (
+    <div className="checkout-item">
+        <div className="image-container">
+            <img src={image} alt="item" />
+        </div>
+
+        <span className="name">{name}</span>
+        <span className="quantity">{quantity}</span>
+        <span className="price">{price}</span>
+        <div className="remove-button">&#1008;</div>
+    </div>
+)
+
+export default CheckoutItem;
